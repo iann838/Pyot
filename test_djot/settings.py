b09723b679752976c79309558b1ed9df74c74a6e
Paaksing <paaksingtech@gmail.com>
2020-08-28 12:29:26 -0400

    changed test-djot Django secret key, throw away anyways

--- test_djot/settings.py
+++ test_djot/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%!$5@5^ha9vw#=#&5k65fn=uc3t+&eu(=#@sk0qzgsawtd3+$p'
+SECRET_KEY = os.environ["SOME_THROW_AWAY_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
