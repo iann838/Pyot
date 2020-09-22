18b77b14c32b2fa0fd81a8e339c604e7fdbf084a
Paaksing <paaksingtech@gmail.com>
2020-09-22 15:19:28 -0400

    Release v1.1.2

--- pyot/__init__.py
+++ pyot/__init__.py
@@ -2,11 +2,14 @@ from . import core, pipeline, utils, models, limiters, stores
 
 try:
     from django.core.exceptions import ImproperlyConfigured
-    from django.conf import settings
-    from importlib import import_module
-    
-    paths = settings.PYOT_SETTINGS
-    
-    for path in paths:
-        import_module(path)
-except (ImportError, ImproperlyConfigured, ModuleNotFoundError, NameError): pass
+    try:
+        from django.conf import settings
+        from importlib import import_module
+        
+        paths = settings.PYOT_SETTINGS
+        
+        for path in paths:
+            import_module(path)
+    except ImproperlyConfigured:
+        pass
+except (ImportError, ModuleNotFoundError, NameError): pass
