ffc0cbb2d43a4445b4e994fc8bed496015fda124
Paaksing <58193352+paaksing@users.noreply.github.com>
2023-01-05 23:48:50 -0400

    New: Support for new SEA platforms

--- pyot/models/tft/base.py
+++ pyot/models/tft/base.py
@@ -8,7 +8,7 @@ MODULE_REPR = 'Teamfight Tactics'
 class PyotRouting:
 
     _regions = {"americas", "europe", "asia", "sea", "esports"}
-    _platforms = {"br1", "eun1", "euw1", "jp1", "kr", "la1", "la2", "na1", "oc1", "tr1", "ru"}
+    _platforms = {"br1", "eun1", "euw1", "jp1", "kr", "la1", "la2", "na1", "oc1", "tr1", "ru", "ph2", "sh2", "th2", "tw2", "vn2"}
     _platform2regions = {
         "br1": "americas",
         "eun1": "europe",
@@ -21,6 +21,11 @@ class PyotRouting:
         "oc1": "sea",
         "tr": "europe",
         "ru": "europe",
+        "ph2": "sea",
+        "sh2": "sea",
+        "th2": "sea",
+        "tw2": "sea",
+        "vn2": "sea",
     }
 
 
